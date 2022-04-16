6c09ae81166f364f0a57a240a116150f7ab19711
Ashley Ufret <103462490+A-Ufret@users.noreply.github.com>
2022-04-16 22:33:21 +0000

    Added comments to pre01 with key vocabulary for reference

--- pre01/Hello.java
+++ pre01/Hello.java
@@ -7,4 +7,14 @@ public class Hello {
         System.out.println("How are you?");
       }
 }
-    
+    //comments - vocab to remember
+//variable - stores some value
+//literals - the value assigned to a variable
+//expression - evaluates to a value
+//operator - work on operands to produce a value (+)
+//operand - the things the operator is working on 
+//class - contains everything and has members
+//methods - do something
+//arguments - what you pass to a method (part of calling)
+//parameter - variables to store arguments (part of definition)
+//new line - \n adds space (do not include a space after \n)
