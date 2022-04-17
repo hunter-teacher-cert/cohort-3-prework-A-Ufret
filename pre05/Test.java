237bdea0bd576a2940ccf03056e0417ee28fc72b
Ashley Ufret <103462490+A-Ufret@users.noreply.github.com>
2022-04-17 03:09:29 +0000

    Changed Testbeer to Test java since I accidentally created it as a regukar file not java, ran test (unsuccessful) review chapter to debug

--- pre05/Test.java
+++ pre05/Test.java
@@ -1,9 +1,11 @@
 import java.io.*;
 import java.util.*;
 
-public class Testbeers lyricsFor(int bearNumber){
+public class Test {
+  public static void main(String args[]){
+         lyricsFor(int bearNumber);
       if (bearNumber > 0){
-         System.out.println(bearNumber + " bottles of beer on the wall,");
+           System.out.println(bearNumber + " bottles of beer on the wall,");
          System.out.println(bearNumber + " bottles of beer,");
          System.out.println("ya' take one down, ya' pass it around,");
          System.out.println(bearNumber-- + "bottles of beer on the wall.");
@@ -15,4 +17,5 @@ public class Testbeers lyricsFor(int bearNumber){
          System.out.println("ya' can't take one down, ya' can't pass it around,");
          System.out.println("'cause there are no more bottles of beer on the wall!");
       }
-   } 
+    }
+}
