8b372e4aab8f584050291f3412e9ee6955d1b243
Ashley Ufret <103462490+A-Ufret@users.noreply.github.com>
2022-04-20 07:27:50 +0000

    Completed Ch6 Ex.3

---
+++ pre05/Divisible.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Divisible { 
+ public static void main(String[] args){
+   System.out.println(isDivisible(5,3));  
+}
+      public static boolean isDivisible(int n, int m) {
+          if (m != 0 && n % m == 0) {
+        return true;
+            } else {
+        return false;
+
+    }       
+        }
+}
+   
+
+
+//method isDivisible needs to return A method to test if the first argument is divisible by the second argument
+      //param n the integer to be divided
+      //param m the integer to divide
+      //return true if m is not `0` and n is divisible by m 
---
+++ pre05/Triangle.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Triangle { 
+   
+  public static void main(String[] args){
+   System.out.println(isTriangle(5,3,7));  
+    }
+
+	public static boolean isTriangle(int length1, int length2, int length3) {	
+
+    if (length1 > (length2 + length3)) {
+			    return false;
+		
+        } else if (length2 > (length1 + length3)) {
+			    return false;
+      
+		    } else if (length3 > (length1 + length2)) {
+			    return false;
+		
+        } else {
+			    return true;
+		}
+	}
+}
+  //Ch6 Ex.3: A program to see if 3 lengths can be a triangle, condition: if the sum of two lengths is greater than the remaining length, the three cannot construct a triangle. 
