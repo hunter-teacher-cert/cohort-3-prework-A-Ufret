7ab320b79254e9b63fdb327b3060251310692fb4
Ashley Ufret <103462490+A-Ufret@users.noreply.github.com>
2022-04-20 07:30:13 +0000

    Labeled parameters for Ch6 Ex 6.2

--- pre05/Divisible.java
+++ pre05/Divisible.java
@@ -10,14 +10,13 @@ public class Divisible {
         return true;
             } else {
         return false;
-
     }       
         }
 }
    
 
 
-//method isDivisible needs to return A method to test if the first argument is divisible by the second argument
+////Ch6 Ex.2 Create method isDivisible needs to return A method to test if the first argument is divisible by the second argument
       //param n the integer to be divided
       //param m the integer to divide
       //return true if m is not `0` and n is divisible by m 
