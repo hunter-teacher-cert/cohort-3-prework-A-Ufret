8176856b62c37110f671972fa8f4bdfb857e5fa2
Ashley Ufret <103462490+A-Ufret@users.noreply.github.com>
2022-04-22 09:06:32 +0000

    work in progress for Array pt2 and Max

--- pre07/Array2.java
+++ pre07/Array2.java
@@ -15,4 +15,4 @@ public static void main(String[] args) {
   }
 }
 
-//Ch8 Ex1 Pt2:  write a method called histogram that takes an int array of scores from 0 to (but not including) 100, and returns a histogram of 100 counters. Generalize it to take the number of counters as an argument.
+//Ch8 Ex1 Pt2:  write a method called histogram tat takes an int array of scores from 0 to (but not including) 100, and returns a histogram of 100 counters. Generalize it to take the number of counters as an argument.
