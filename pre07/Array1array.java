33b81c3465b4a9b01d212a2bbcab42239feda5b6
Ashley Ufret <103462490+A-Ufret@users.noreply.github.com>
2022-04-22 00:43:05 +0000

    in progress pre07

---
+++ pre07/Array1array.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Array1array { 
+
+  public static void main(String[] args) {
+        double[] squareArry = powArray(new double[]{2, 3, 4, 5});
+    }
+
+    public static double[] powArray(double a[]){
+        double[] b = new double[a.length];
+        for (int i = 0; i < a.length; i++) {
+            b[i] = Math.pow(a[i], 3.0);//given power 3.0
+            System.out.println(Arrays.toString(b));
+        }
+        return b;
+      
+    }
+}
+
+
+//Ch8 Ex1 Pt1: Starting with the code in Section 8.6 (pasted below), write a method called powArray that takes a double array, a, and returns a new array that contains the elements of a squared. Generalize it to take a second argument and raise the elements of a to the given power.
+/*                       Code in section 8.6:
+import java.io.*;
+import java.util.*;
+
+public class Ch7Question1 { 
+public static void main(String[] args) {
+  }
+  public static int search(double[] a, double target) {
+    for (int i = 0; i < a.length; i++) {
+        if (a[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
+  }             */
--- pre07/Max.java
+++ pre07/Max.java
@@ -4,11 +4,21 @@ import java.util.*;
 public class Max { 
 
 public static void main(String[] args) {
-
-
-
-
+  int[] arr = new int[] arr{3,10,18,7,74};
+   printArray(array);
   }
-}
+
+public static int indexOfMax(int[] arr, int lowIndex, int highIndex) {
+		int indexOfMax = lowIndex;
+		
+		for (int i = lowIndex+1; i <= highIndex; i++) {
+			if (arr[i] > arr[indexOfMax]) {
+				indexOfMax = i;
+			}
+		}
+		return indexOfMax; 
+	}
+  }
+
 
 //Ch8 Exercise 4   Write a method called indexOfMax that takes an array of integers and returns the index of the largest element. Can you write this method using an enhanced for loop? Why or why not?
