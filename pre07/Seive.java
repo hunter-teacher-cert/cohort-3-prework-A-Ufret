e5771b8d71a8c10817a4044d3386fb0f53069757
Ashley Ufret <103462490+A-Ufret@users.noreply.github.com>
2022-04-23 06:32:57 +0000

    Pre07 all done!!

--- pre07/Seive.java
+++ pre07/Seive.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Seive { 
 
 public static void main(String[] args) {
-         int a[]= {4,10,-3,-44,15,75,60,12,51,19};
+         int a[]= {4,10,-3,-44,15,75,60,12,51,19};  
 
     System.out.println((indexOfMax(a)));
   }
@@ -21,3 +21,5 @@ public static void main(String[] args) {
 
 //Ch8 Exercise 5   The Sieve of Eratosthenes is “a simple, ancient algorithm for finding all prime numbers up to any given limit,” which you can read about at https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes.
 //Write a method called sieve that takes an integer parameter, n, and returns a boolean array that indicates, for each number from 0 to n - 1, whether the number is prime.
+
+//if time permits://add scanner to replace list of integers with input value and set say "Yes, "it" is a prime number." OR "No, "it" is not a prime number."
